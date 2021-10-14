e80b8ea53acc459bf275118d37d591225c850172
Rohan Phillips <rohan@solutions-insurance.com>
2021-10-14 15:49:15 -0500

    add photos testing

--- lean-photos/src/reducers/photos.test.js
+++ lean-photos/src/reducers/photos.test.js
@@ -30,8 +30,17 @@ test('should add photos', () => {
       photosLoaded: true,
       photoList: photos,
       albums: { 
-         1: {albumId: 1, id: 1, title: 'accusamus beatae ad facilis cum similique qui sunt', url: 'https://via.placeholder.com/600/92c952', thumbnailUrl: 'https://via.placeholder.com/150/92c952'},
-         2: {albumId: 1, id: 2, title: 'reprehenderit est deserunt velit ipsam', url: 'https://via.placeholder.com/600/771796', thumbnailUrl: 'https://via.placeholder.com/150/771796'}
+         1: [{albumId: 1, id: 1, title: 'accusamus beatae ad facilis cum similique qui sunt', url: 'https://via.placeholder.com/600/92c952', thumbnailUrl: 'https://via.placeholder.com/150/92c952'},
+         {albumId: 1, id: 2, title: 'reprehenderit est deserunt velit ipsam', url: 'https://via.placeholder.com/600/771796', thumbnailUrl: 'https://via.placeholder.com/150/771796'}]
       }
+   })
+})
+
+test('should set photosLoaded', () => {
+   const previousState = initialState;
+   expect(reducer(previousState, setPhotosLoaded(true))).toEqual({
+      photosLoaded: true,
+      photoList: [],
+      albums: {}
    })
 })
